2d4b0d080966c583de399fc92af026e23aee00ee
SnehaMol-01 <snehamol977@gmail.com>
2025-06-06 12:53:53 +0530

    First changes

--- src/com/training/Train.java
+++ src/com/training/Train.java
@@ -1,5 +1,13 @@
 package com.training;
 
 public class Train {
-
+	public void main() {
+		System.out.println("hello");
+		
+		
+	}
+public static void main(String[] args) {
+	Train t=new Train();
+	t.main();
+}
 }
